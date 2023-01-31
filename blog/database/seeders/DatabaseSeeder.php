d4c39ae5fdfcdc225870e57a77e0867ddaf76fcd
Marco <93841465+Marco1080@users.noreply.github.com>
2023-01-31 23:13:06 +0000

    Update 1.
    Llenar con datos falsos la bbdd de un blog (https://codersfree.com/courses-status/aprende-laravel-9-desde-cero/llenar-con-datos-falsos-la-bbdd-de-un-blog)

--- blog/database/seeders/DatabaseSeeder.php
+++ blog/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,11 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\Tag;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,11 +19,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        Storage::deleteDirectory('public/posts');
+        Storage::makeDirectory('public/posts');
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->call(UserSeeder::class);
+        Category::factory(4)->create();
+        Tag::factory(8)->create();
+        $this->call(PostSeeder::class);
     }
 }
