98fe9d471ddf5d144e2fdc5865f210fdb30a070a
Marco <93841465+Marco1080@users.noreply.github.com>
2023-02-23 17:57:36 +0000

    Formulario para crear nuevo post added

--- blog/app/Http/Controllers/Admin/PostController.php
+++ blog/app/Http/Controllers/Admin/PostController.php
@@ -6,6 +6,8 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Post;
+use App\Models\Category;
+use App\Models\Tag;
 
 class PostController extends Controller
 {
@@ -26,7 +28,10 @@ class PostController extends Controller
      */
     public function create()
     {
-        return view('admin.posts.create');
+        $categories = Category::pluck('name', 'id');
+        $tags = Tag::all();
+
+        return view('admin.posts.create', compact('categories', 'tags'));
     }
 
     /**
--- blog/resources/views/admin/posts/index.blade.php
+++ blog/resources/views/admin/posts/index.blade.php
@@ -3,7 +3,8 @@
 @section('title', 'Dashboard')
 
 @section('content_header')
-    <h1>Mostrar potsts</h1>
+    <a class="brn btn-secondary btn-sm float-right" href="{{route('admin.posts.create')}}">Nuevo post</a>
+    <h1>Listado de posts</h1>
 @stop
 
 @section('content')
